8a516bdb66b7fbf19f77b200ed7bfeaf34375514
SunsRise <mm.gunes.mmg@gmail.com>
2021-10-08 08:35:21 +0300

    Add project files.

---
+++ Crosplatform.cpp
@@ -0,0 +1,12 @@
+﻿// Crosplatform.cpp : Defines the entry point for the application.
+//
+
+#include "Crosplatform.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
